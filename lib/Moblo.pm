8bdcb80353c3f80344be44ce475a5ee5f02dd4d8
casey <caseycole589@gmail.com>
2015-03-11 19:58:11 -0500

    made first route template and set the default layout

--- lib/Moblo.pm
+++ lib/Moblo.pm
@@ -8,13 +8,28 @@ sub startup {
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
 
+  $self->defaults(layout => 'base');
+
   # Router
   my $r = $self->routes;
 
+  #template is directly renderd in route definition
+  $r->get('/')->to(template => 'main/index');
+
+  # but can be used to manaully render data with a controller like this
+  # $r->get('/')->to('main#index');
+  # or 
+  # $r->route('/')->via('GET')->to(controller => 'Main', action => 'index')
+
+
+
+
+  #example came with
   # Normal route to controller
   #$r->get('/')->to('example#welcome');
   # could also be written as
-  $r->route('/')->via('GET')->to(controller => 'Example', action => 'welcome'); 
+  #$r->route('/')->via('GET')->to(controller => 'Example', action => 'welcome'); 
+
 }
 
 1;
---
+++ lib/Moblo/Controller/Main.pm
@@ -0,0 +1,10 @@
+package Moblo::Main;
+use Mojo::Base 'Mojolicious::Controller';
+
+	#actions go here
+sub index {
+	
+	my $self = shift;
+	#this action forces the rendering of templates/main/index
+	$self->render('main/index');	
+}
