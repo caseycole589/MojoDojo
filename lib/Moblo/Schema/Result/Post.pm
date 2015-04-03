9e8e25b65d46d480080e61c36d1ca0d5c6c1534c
casey <caseycole589@gmail.com>
2015-04-02 19:07:00 -0500

    succesfully compiles with dbix relationships

--- lib/Moblo/Schema/Result/Post.pm
+++ lib/Moblo/Schema/Result/Post.pm
@@ -1,34 +1,56 @@
 package Moblo::Schema::Result::Post;
 use base qw/DBIx::Class::Core/;
 
- # Associated table in database
- __PACKAGE__->table('posts');
+#has_many realationship (that is zero or more records in the foriegn tabel post)
+#has_one relationship Posts -> User(one autor per post)
+#belongs_to relationship Comments -> Posts Comments -> Users each comment has a foreign key to one user and post
+#relation ships are added for each table to the bottom of each associated result class
 
- # Column definition
- __PACKAGE__->add_columns(
+# Associated table in database
+__PACKAGE__->table('posts');
 
-      id => {
-          data_type => 'integer',
-          is_auto_increment => 1,
-      },
+# Column definition
+__PACKAGE__->add_columns(
 
-      author => {
-          data_type => 'text',
-      },
+    id => {
+        data_type => 'integer',
+        is_auto_increment => 1,
+    },
 
-      title => {
-          data_type => 'text',
-      },
+    author_id => {
+        data_type => 'text',
+    },
 
-      content => {
-          data_type => 'text',
-      },
+    title => {
+        data_type => 'text',
+    },
 
-      date_published => {
-          data_type => 'datetime',
-      },
+    content => {
+        data_type => 'text',
+    },
 
-  );
+    date_published => {
+        data_type => 'datetime',
+    },
+
+);
 
 # Tell DBIC that 'id' is the primary key
 __PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(  
+  #Name of the accessor for this relation
+  author =>
+  #Foreign result class 
+  'Moblo::Schema::Result::User',
+  #Foreign key in THIS table 
+  'author_id'
+);
+
+__PACKAGE__->has_many(
+  comments =>
+  'Moblo::Schema::Result::Comment',
+  'post_id'
+);
+
+1;
