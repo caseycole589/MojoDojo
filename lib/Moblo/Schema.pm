54f336858d6b5970f0c80c5e1268c1c77d0cd8db
casey <caseycole589@gmail.com>
2015-04-20 18:24:29 -0500

    fixed login not working

--- lib/Moblo/Schema.pm
+++ lib/Moblo/Schema.pm
@@ -7,7 +7,7 @@ use warnings;
 use base 'DBIx::Class::Schema';
 
 #schema database version
-our $VERSION = 5;
+our $VERSION = 6;
 
 # This will load any classes within
 # Moblo::Schema::Result and Moblo::Schema::ResultSet (if any)
